acfcec6c24e651c7fcb044f9c80bbf9967f69fa3
David Amato <daveamato@live.com>
2018-11-16 09:16:49 -0500

    changes

--- app.js
+++ app.js
@@ -27,12 +27,33 @@ const requestCache = (duration) => {
 /** ROUTES **/
 /* home */
 app.get('/', (req, res) => {
-  const text = '<h1>OpenLoad.CO API project</h1><p>This is just a scraper, if you have content you wish to be removed, please contact them (openload.co), not me, at their email: <a href="mailto:dmca@openload.co">dmca@openload.co</a>.</p><p>Full source code available on Github: <a href="https://www.github.com/milankragujevic/openload-api/" target="_blank">openload-api.git</a>. </p><hr><p>Copyright &copy; 2018 Milan Kragujević. Some rights reserved. By using this API you may be committing copyright infringement. I am not responsible for the contents of the API. </p>'
+  const text = '<h1>YouTube-DL API project</h1><p>This is just a demonstration, <p>Full original source code available on Github: <a href="https://www.github.com/milankragujevic/openload-api/" target="_blank">openload-api.git</a>. </p><hr><p>Copyright &copy; 2018 David Amato. Some rights reserved. By using this API you may be committing copyright infringement. I am not responsible for the contents or original creation of the API. </p>'
   res.send(text)
 })
 
+app.get('/:dlUrl', requestCache(60 * 60 * 12), (req, res) => {
+  if (req.params.videoId === '' || req.params.videoId === 'favicon.ico') { return }
+
+  let url = `${req.params.dlUrl}`
+  youtubedl.getInfo(url, (err, info) => {
+    if (err) {
+      res.send({ status: false, error: 'Unknown error occurred!' })
+    }
+
+    res.send({
+      success: true,
+      data: {
+        id: info.id||'None',
+        title: info.title||'None',
+        stream: info.url||'None',
+        thumbnail: info.thumbnail||'None'
+      }
+    })
+  })
+})
+
 /* popular items, with pagination */
-app.get('/:videoId', requestCache(60 * 60 * 12), (req, res) => {
+app.get('/ol/:videoId', requestCache(60 * 60 * 12), (req, res) => {
   if (req.params.videoId === '' || req.params.videoId === 'favicon.ico') { return }
 
   let url = `https://openload.co/embed/${req.params.videoId}/`
