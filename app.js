588aa7085f088e2d8b4078b9205d79828c5dbdd0
David Amato <daveamato@live.com>
2018-11-17 18:13:10 -0500

    f

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/get/*', requestCache(60 * 60 * 12), (req, res) => {
-  if (req.params.dlUrl === '' || req.params.dlUrl === 'favicon.ico') { return }
+  //if (req.params.dlUrl === '' || req.params.dlUrl === 'favicon.ico') { return }
 
   //let url = `${req.params.dlUrl}`
   let url = req.params[0]
@@ -58,15 +58,15 @@ app.get('/get/*', requestCache(60 * 60 * 12), (req, res) => {
 app.get('/play/*', (req, res) => {
 
   let path = req.params[0]
-  logger.info('getting', { url: path })
+  //logger.info('getting', { url: path })
   
   const ytopts = ['--source-address', req.ip];
   
-  youtubedl.getInfo(path, ['-g'], (err, info) => {
+  youtubedl.getInfo(path, (err, info) => {
     if (err) {
       res.send({ status: false, error: err })
     }
-    logger.info('resolved', { 'url': info.url })
+    //logger.info('resolved', { 'url': info.url })
     res.redirect(info.url)
     
     //res.setHeader('Content-Type', 'application/x-mpegURL');
