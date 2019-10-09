54f58a2ee048f22cfc07bb809893031250c662cb
Dave Amato <daveamato@live.com>
2019-10-09 16:00:14 +0000

    up

--- app.js
+++ app.js
@@ -1,78 +1,95 @@
-import express from 'express'
-import youtubedl from 'youtube-dl'
-import mcache from 'memory-cache'
-import logger from 'heroku-logger'
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _youtubeDl = require('youtube-dl');
+
+var _youtubeDl2 = _interopRequireDefault(_youtubeDl);
+
+var _memoryCache = require('memory-cache');
+
+var _memoryCache2 = _interopRequireDefault(_memoryCache);
+
+var _herokuLogger = require('heroku-logger');
+
+var _herokuLogger2 = _interopRequireDefault(_herokuLogger);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 //import https from 'https'
 
-const app = express()
+var app = (0, _express2.default)();
 
 /** CONFIG **/
-const port = process.env.PORT || 4000
+//var port = process.env.PORT || 3000;
+var port = 3000;
 
-const requestCache = (duration) => {
-  return (req, res, next) => {
-    let key = `__express__${req.originalUrl}` || req.url
-    let cachedBody = mcache.get(key)
+var requestCache = function requestCache(duration) {
+  return function (req, res, next) {
+    var key = '__express__' + req.originalUrl || req.url;
+    var cachedBody = _memoryCache2.default.get(key);
     if (cachedBody) {
-      res.send(cachedBody)
+      res.send(cachedBody);
     } else {
-      res.sendResponse = res.send
-      res.send = (body) => {
-        mcache.put(key, body, duration)
-        res.sendResponse(body)
-      }
-      next()
+      res.sendResponse = res.send;
+      res.send = function (body) {
+        _memoryCache2.default.put(key, body, duration);
+        res.sendResponse(body);
+      };
+      next();
     }
-  }
-}
+  };
+};
 
 /** ROUTES **/
 /* home */
-app.get('/', (req, res) => {
-  const text = '<h1>YouTube-DL API</h1><p>This is just a demonstration, not for public use.</p><p>Full original source code available on Github: <a href="https://www.github.com/daveamato/openload-api/" target="_blank">openload-api</a>. </p><hr><p>Copyright &copy; 2018 David Amato. Some rights reserved. By using this API you may be committing copyright infringement. I am not responsible for the contents or original creation of the API.</p>'
-  res.send(text)
-})
+app.get('/', function (req, res) {
+  var text = '<h1>YouTube-DL API</h1><p>This is just a demonstration, not for public use.</p><p>Full original source code available on Github: <a href="https://www.github.com/daveamato/openload-api/" target="_blank">openload-api</a>. </p><hr><p>Copyright &copy; 2018 David Amato. Some rights reserved. By using this API you may be committing copyright infringement. I am not responsible for the contents or original creation of the API.</p>';
+  res.send(text);
+});
 
-app.get('/get/*', requestCache(60 * 60 * 12), (req, res) => {
+app.get('/get/*', requestCache(60 * 60 * 12), function (req, res) {
   //if (req.params.dlUrl === '' || req.params.dlUrl === 'favicon.ico') { return }
 
   //let url = `${req.params.dlUrl}`
-  let url = req.params[0]
-  youtubedl.getInfo(url, (err, info) => {
+  var url = req.params[0];
+  _youtubeDl2.default.getInfo(url, function (err, info) {
     if (err) {
-      res.send({ status: false, error: 'Unknown error occurred!' })
+      res.send({ status: false, error: 'Unknown error occurred!' });
     }
-    
-    res.send(info.url)
-/*
-    res.send({
-        id: info.id ? info.id : 'None',
-        title: info.title ? info.title : 'None',
-        stream: info.url ? info.url : 'None',
-        thumbnail: info.thumbnail ? info.thumbnail : 'None'
-      })
-*/
-  })
-})
 
-app.get('/play/*', (req, res) => {
+    res.send(info.url);
+    /*
+        res.send({
+            id: info.id ? info.id : 'None',
+            title: info.title ? info.title : 'None',
+            stream: info.url ? info.url : 'None',
+            thumbnail: info.thumbnail ? info.thumbnail : 'None'
+          })
+    */
+  });
+});
 
-  let path = req.params[0]
+app.get('/play/*', function (req, res) {
+
+  var path = req.params[0];
   //logger.info('getting', { url: path })
-  
-  const ytopts = ['--source-address', req.ip];
-  
-  youtubedl.getInfo(path, (err, info) => {
+
+  var ytopts = ['--source-address', req.ip];
+
+  _youtubeDl2.default.getInfo(path, function (err, info) {
     if (err) {
-      res.send({ status: false, error: err })
+      res.send({ status: false, error: err });
     }
     //logger.info('resolved', { 'url': info.url })
-    res.redirect(info.url)
-    
+    res.redirect(info.url);
+
     //res.setHeader('Content-Type', 'application/x-mpegURL');
     //res.attachment(info._filename);
     //https.get(path).pipe(res);
-    
+
     /*
     let stat = fs.statSync(info.url)
     let fileSize = stat.size
@@ -113,16 +130,18 @@ app.get('/play/*', (req, res) => {
       }
     }
     */
-  })
+  });
 });
 
-app.get('/ol/:videoId', requestCache(60 * 60 * 12), (req, res) => {
-  if (req.params.videoId === '' || req.params.videoId === 'favicon.ico') { return }
+app.get('/ol/:videoId', requestCache(60 * 60 * 12), function (req, res) {
+  if (req.params.videoId === '' || req.params.videoId === 'favicon.ico') {
+    return;
+  }
 
-  let url = `https://openload.co/embed/${req.params.videoId}/`
-  youtubedl.getInfo(url, (err, info) => {
+  var url = 'https://openload.co/embed/' + req.params.videoId + '/';
+  _youtubeDl2.default.getInfo(url, function (err, info) {
     if (err) {
-      res.send({ status: false, error: 'Unknown error occurred!' })
+      res.send({ status: false, error: 'Unknown error occurred!' });
     }
 
     res.send({
@@ -133,12 +152,12 @@ app.get('/ol/:videoId', requestCache(60 * 60 * 12), (req, res) => {
         stream: info.url ? info.url : 'None',
         thumbnail: info.thumbnail ? info.thumbnail : 'None'
       }
-    })
-  })
-})
+    });
+  });
+});
 
 /** LISTEN **/
 app.listen(port, function () {
-     console.log("Running API on port " + port);
-     logger.info('started', { success: true })
+  console.log("Running API on port " + port);
+  _herokuLogger2.default.info('started', { success: true });
 });
--- dist/app.js
+++ dist/app.js
@@ -12,12 +12,18 @@ var _memoryCache = require('memory-cache');
 
 var _memoryCache2 = _interopRequireDefault(_memoryCache);
 
+var _herokuLogger = require('heroku-logger');
+
+var _herokuLogger2 = _interopRequireDefault(_herokuLogger);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+//import https from 'https'
 
 var app = (0, _express2.default)();
 
 /** CONFIG **/
-var port = process.env.PORT || 4000;
+var port = process.env.PORT || 3000;
 
 var requestCache = function requestCache(duration) {
   return function (req, res, next) {
@@ -39,12 +45,94 @@ var requestCache = function requestCache(duration) {
 /** ROUTES **/
 /* home */
 app.get('/', function (req, res) {
-  var text = '<h1>OpenLoad.CO API project</h1><p>This is just a scraper, if you have content you wish to be removed, please contact them (openload.co), not me, at their email: <a href="mailto:dmca@openload.co">dmca@openload.co</a>.</p><p>Full source code available on Github: <a href="https://www.github.com/milankragujevic/openload-api/" target="_blank">openload-api.git</a>. </p><hr><p>Copyright &copy; 2018 Milan Kragujević. Some rights reserved. By using this API you may be committing copyright infringement. I am not responsible for the contents of the API. </p>';
+  var text = '<h1>YouTube-DL API</h1><p>This is just a demonstration, not for public use.</p><p>Full original source code available on Github: <a href="https://www.github.com/daveamato/openload-api/" target="_blank">openload-api</a>. </p><hr><p>Copyright &copy; 2018 David Amato. Some rights reserved. By using this API you may be committing copyright infringement. I am not responsible for the contents or original creation of the API.</p>';
   res.send(text);
 });
 
-/* popular items, with pagination */
-app.get('/:videoId', requestCache(60 * 60 * 12), function (req, res) {
+app.get('/get/*', requestCache(60 * 60 * 12), function (req, res) {
+  //if (req.params.dlUrl === '' || req.params.dlUrl === 'favicon.ico') { return }
+
+  //let url = `${req.params.dlUrl}`
+  var url = req.params[0];
+  _youtubeDl2.default.getInfo(url, function (err, info) {
+    if (err) {
+      res.send({ status: false, error: 'Unknown error occurred!' });
+    }
+
+    res.send(info.url);
+    /*
+        res.send({
+            id: info.id ? info.id : 'None',
+            title: info.title ? info.title : 'None',
+            stream: info.url ? info.url : 'None',
+            thumbnail: info.thumbnail ? info.thumbnail : 'None'
+          })
+    */
+  });
+});
+
+app.get('/play/*', function (req, res) {
+
+  var path = req.params[0];
+  //logger.info('getting', { url: path })
+
+  var ytopts = ['--source-address', req.ip];
+
+  _youtubeDl2.default.getInfo(path, function (err, info) {
+    if (err) {
+      res.send({ status: false, error: err });
+    }
+    //logger.info('resolved', { 'url': info.url })
+    res.redirect(info.url);
+
+    //res.setHeader('Content-Type', 'application/x-mpegURL');
+    //res.attachment(info._filename);
+    //https.get(path).pipe(res);
+
+    /*
+    let stat = fs.statSync(info.url)
+    let fileSize = stat.size
+    let range = req.headers.range
+    if (range) {
+      let parts = range.replace(/bytes=/, "").split("-")
+      let start = parseInt(parts[0], 10)
+      let end = parts[1] 
+        ? parseInt(parts[1], 10)
+        : fileSize-1
+      let chunksize = (end-start)+1
+      let file = fs.createReadStream(path, {start, end})
+      let head = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'video/mp4',
+      }
+      res.writeHead(206, head);
+      file.pipe(res);
+    } else {
+      let head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+      }
+      res.writeHead(200, head)
+      fs.createReadStream(path).pipe(res)
+    }
+    */
+    /*
+    obJ = {
+      success: true,
+      data: {
+        id: info.id||'None',
+        title: info.title||'None',
+        stream: info.url||'None',
+        thumbnail: info.thumbnail||'None'
+      }
+    }
+    */
+  });
+});
+
+app.get('/ol/:videoId', requestCache(60 * 60 * 12), function (req, res) {
   if (req.params.videoId === '' || req.params.videoId === 'favicon.ico') {
     return;
   }
@@ -58,15 +146,17 @@ app.get('/:videoId', requestCache(60 * 60 * 12), function (req, res) {
     res.send({
       success: true,
       data: {
-        id: info.id,
-        title: info.title,
-        stream: info.url,
-        thumbnail: info.thumbnail
+        id: info.id ? info.id : 'None',
+        title: info.title ? info.title : 'None',
+        stream: info.url ? info.url : 'None',
+        thumbnail: info.thumbnail ? info.thumbnail : 'None'
       }
     });
   });
 });
 
 /** LISTEN **/
-app.listen(port);
-console.log('Listening on: ' + port);
+app.listen(port, function () {
+  console.log("Running API on port " + port);
+  _herokuLogger2.default.info('started', { success: true });
+});
